54584a03c17b39146d050ccaf89698a0e74e8ccd
Hussein <mnasg32@gmail.com>
2025-12-31 13:18:33 +0300

    Add configuration settings for API and bot

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+# ⚙️ إعدادات السورس (Configuration)
+API_ID = int(os.getenv("API_ID", "22439859"))
+API_HASH = os.getenv("API_HASH", "312858aa733a7bfacf54eede0c275db4")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8586272670:AAHJ2dl_bJlCC4gvWQyyJksq36-FsPLCoN0")
+REQUIRED_CHANNEL = os.getenv("REQUIRED_CHANNEL", "iomk3")  
+SUPPORT_USER = os.getenv("SUPPORT_USER", "iomk0")
